8ec476132018881285e7b88553bac7862712a969
github-action-benchmark <github@users.noreply.github.com>
2023-06-05 18:41:33 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 1e7cc8b42abcdca77e22fe667cbd140bfe8b097a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685990492107,
+  "lastUpdate": 1685990493005,
   "repoUrl": "https://github.com/ledwards2225/github-action-benchmark",
   "entries": {
     "Go Benchmark": [
@@ -36,6 +36,46 @@ window.BENCHMARK_DATA = {
             "value": 48765,
             "unit": "ns/op",
             "extra": "30000 times\n2 procs"
+          }
+        ]
+      }
+    ],
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "l.edwards.d@gmail.com",
+            "name": "ledwards2225",
+            "username": "ledwards2225"
+          },
+          "committer": {
+            "email": "l.edwards.d@gmail.com",
+            "name": "ledwards2225",
+            "username": "ledwards2225"
+          },
+          "distinct": true,
+          "id": "1e7cc8b42abcdca77e22fe667cbd140bfe8b097a",
+          "message": "testing",
+          "timestamp": "2023-06-05T11:41:02-07:00",
+          "tree_id": "d6f525bf188967936d7565455003e7ebfa866522",
+          "url": "https://github.com/ledwards2225/github-action-benchmark/commit/1e7cc8b42abcdca77e22fe667cbd140bfe8b097a"
+        },
+        "date": 1685990491751,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 52715.16382569605,
+            "unit": "iter/sec",
+            "range": "stddev: 5.222848285004587e-7",
+            "extra": "mean: 18.969873702878434 usec\nrounds: 36232"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 432.9977233586923,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000007245328327048085",
+            "extra": "mean: 2.3094809650340977 msec\nrounds: 429"
           }
         ]
       }
